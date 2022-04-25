69ad0200177c461af01d24698d2e7726a5d686b9
parzivalElijah <aithammouk40@gmail.com>
2022-04-25 15:34:19 +0100

    added a form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,27 +13,36 @@ import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import mosque from "../images/mosque.jpg";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object({
+  email: yup
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string("Enter your password")
+    .min(8, "Password should be of minimum 8 characters length")
+    .required("Password is required"),
+});
+
 export default function Login() {
   const userRef = useRef();
-  const [username, setUsername] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [errMsg, setErrMsg] = useState("");
-  const [success, setSuccess] = useState(false);
+
   let history = useNavigate();
 
-  useEffect(() => {
-    userRef.current.focus();
-  }, []);
-
-  useEffect(() => {
-    setErrMsg("");
-  }, [username, password]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(username);
-    history("/dashboard");
-  };
+  const formik = useFormik({
+    initialValues: {
+      email: "foobar@example.com",
+      password: "foobar",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      //alert(JSON.stringify(values, null, 2));
+      history("/dashboard");
+    },
+  });
 
   return (
     <Grid container component="main" sx={{ height: "100vh" }}>
@@ -69,7 +78,7 @@ export default function Login() {
           <Box
             component="form"
             noValidate
-            onSubmit={handleSubmit}
+            onSubmit={formik.handleSubmit}
             sx={{ mt: 1 }}
           >
             <TextField
@@ -77,22 +86,26 @@ export default function Login() {
               required
               fullWidth
               ref={userRef}
-              onChange={(e) => setUsername(e.target.value)}
-              value={username == null ? "" : username}
+              onChange={formik.handleChange}
+              value={formik.values.email}
               id="email"
               label="Email Address"
               autoComplete="email"
               autoFocus
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
             />
             <TextField
               margin="normal"
               required
               fullWidth
-              onChange={(e) => setPassword(e.target.value)}
-              value={password == null ? "" : password}
+              onChange={formik.handleChange}
+              value={formik.values.password}
               label="Password"
               type="password"
               id="password"
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
               autoComplete="current-password"
             />
             <FormControlLabel
